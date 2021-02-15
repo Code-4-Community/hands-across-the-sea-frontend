e439c9e1835abdc8fe4511df815466a33832fd1f
Max Sebso <maxsebso@gmail.com>
2021-02-14 23:21:53 -0500

    formatting done, but styling needs to be added

---
+++ src/components/login-signup/LoginContainer.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Row, Col, Typography } from 'antd';
+import { ContentContainer, SectionTitle, Outer, Inner } from '..';
+import styled from 'styled-components';
+
+const { Title } = Typography;
+
+/*
+const FormTitle = styled(Title)`
+  text-align: center;
+  margin: 24px 0px 0px 0px;
+`;
+
+const ChildrenContainer = styled.div`
+  max-width: 960px;
+`;*/
+
+const LoginTitle = styled(Title)`
+  font-weight: bold;
+  text-align: center;
+`;
+
+const Container = styled(ContentContainer)`
+  max-width: 448px;
+`;
+
+const LoginContainer: React.FC = (props) => {
+  return (
+    <Container>
+      <LoginTitle level={2}>Log In</LoginTitle>
+      <Outer>
+        <Inner>{props.children}</Inner>
+      </Outer>
+    </Container>
+  );
+};
+
+export default LoginContainer;
--- src/containers/login/Login.tsx
+++ src/containers/login/Login.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import { Button, Form, Input, Typography } from 'antd';
+import { Button, Form, Input, Typography, Row, Col } from 'antd';
 import { Link } from 'react-router-dom';
 import { login } from '../../auth/ducks/thunks';
 import { connect, useDispatch } from 'react-redux';
 import { AsyncRequestKinds } from '../../utils/asyncRequest';
 import { C4CState } from '../../store';
 import { UserAuthenticationReducerState } from '../../auth/ducks/types';
+import LoginContainer from '../../components/login-signup/LoginContainer';
+import styled from 'styled-components';
 
 const { Title, Paragraph } = Typography;
+
+const SubmitButton = styled(Button)`
+  border-radius: 5px;
+  width: 192px;
+`;
+
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SubText = styled(Paragraph)`
+  font-size: 12px;
+  font-weight: 400;
+  color: #767a7d;
+`;
 
 type LoginProps = UserAuthenticationReducerState;
 
@@ -24,45 +43,83 @@ const Login: React.FC<LoginProps> = ({ tokens }) => {
         <meta name="description" content="Description goes here." />
       </Helmet>
       <div className="content-container">
-        <Title>Login</Title>
-        <Form name="basic" onFinish={onFinish}>
-          <Form.Item
-            label="Email"
-            name="email"
-            rules={[{ required: true, message: 'Please input your email!' }]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[{ required: true, message: 'Please input your password!' }]}
-          >
-            <Input.Password />
-          </Form.Item>
-          <Paragraph>
-            Need an account? Sign up{' '}
-            <Link to="/signup" component={Typography.Link}>
-              here
-            </Link>
-            !
-          </Paragraph>
-          <Paragraph>
-            Forgot your password? Click{' '}
-            <Link to="/" component={Typography.Link}>
-              here
-            </Link>{' '}
+        <LoginContainer>
+          <Form name="login" onFinish={onFinish}>
+            <Row>
+              <Col span={24}>
+                <Paragraph>Email:</Paragraph>
+              </Col>
+            </Row>
+            <Row>
+              <Col span={24}>
+                <Form.Item
+                  name="email"
+                  rules={[
+                    { required: true, message: 'Please input your email!' },
+                  ]}
+                >
+                  <Input />
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row>
+              <Col span={24}>
+                <Paragraph>Password:</Paragraph>
+              </Col>
+            </Row>
+            <Row>
+              <Col span={24}>
+                <Form.Item
+                  name="password"
+                  rules={[
+                    { required: true, message: 'Please input your password!' },
+                  ]}
+                >
+                  <Input.Password />
+                </Form.Item>
+              </Col>
+            </Row>
+            {/*
+            <Paragraph>
+              Need an account? Sign up{' '}
+              <Link to="/signup" component={Typography.Link}>
+                here
+              </Link>
+              !
+            </Paragraph>
+            <Paragraph>
+              Forgot your password? Click{' '}
+              <Link to="/" component={Typography.Link}>
+                here
+              </Link>{' '}
             to reset it.
-          </Paragraph>
-          {tokens.kind === AsyncRequestKinds.Failed && (
-            <Paragraph>{tokens.error}</Paragraph>
-          )}
-          <Form.Item>
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
+            </Paragraph>
+            */}
+            {tokens.kind === AsyncRequestKinds.Failed && (
+              <Paragraph>{tokens.error}</Paragraph>
+            )}
+            <Row>
+              <Col span={24}>
+                <Form.Item>
+                  <Center>
+                    <SubmitButton type="primary" htmlType="submit">
+                      Submit
+                    </SubmitButton>
+                  </Center>
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row>
+              <Col span={24}>
+                <Center>
+                  <Link to="/not-found" component={Typography.Link}>
+                    <SubText>Trouble logging in?</SubText>
+                  </Link>
+                </Center>
+              </Col>
+            </Row>
+          </Form>
+        </LoginContainer>
       </div>
     </>
   );
