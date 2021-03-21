045042756904b1de32468d98fd8ab36f5ee07982
Jack Blanc <jackblanc@hey.com>
2021-03-21 12:42:14 -0400

    add client, update thunk

--- src/api/protectedApiClient.ts
+++ src/api/protectedApiClient.ts
@@ -1,18 +1,20 @@
 import AppAxiosInstance from '../auth/axios';
-
-// add to interface
-// add to enum
-// include function in object Client
+import {
+  SchoolRequest,
+  SchoolResponse,
+} from '../containers/schoolInfo/ducks/types';
 
 export interface ProtectedApiClient {
   readonly changePassword: (request: {
     currentPassword: string;
     newPassword: string;
   }) => Promise<void>;
+  readonly createSchool: (request: SchoolRequest) => Promise<SchoolResponse>;
 }
 
 enum ProtectedApiClientRoutes {
   CHANGE_PASSWORD = '/api/v1/protected/user/change_password',
+  SCHOOL_INFO = '/api/v1/protected/schools',
 }
 
 const changePassword = (request: {
@@ -27,8 +29,15 @@ const changePassword = (request: {
     .catch((e) => e);
 };
 
+const createSchool = (request: SchoolRequest): Promise<SchoolResponse> => {
+  return AppAxiosInstance.post(ProtectedApiClientRoutes.SCHOOL_INFO, request)
+    .then((r) => r.data)
+    .catch((e) => e);
+};
+
 const Client: ProtectedApiClient = Object.freeze({
   changePassword,
+  createSchool,
 });
 
 export default Client;
--- src/containers/schoolInfo/ducks/thunks.ts
+++ src/containers/schoolInfo/ducks/thunks.ts
@@ -5,18 +5,16 @@ import {
 } from '../../../auth/ducks/types';
 import { authenticateUser } from '../../../auth/ducks/actions';
 import AppAxiosInstance from '../../../auth/axios';
-import {SchoolRequest} from './types';
+import { SchoolRequest, SchoolResponse } from "./types";
 
 export const createSchool = (
   schoolRequest: SchoolRequest,
 ): UserAuthenticationThunkAction<void> => {
-  return (dispatch, getState, { authClient }): Promise<void> => {
+  return (dispatch, getState, { protectedApiClient }): Promise<void> => {
     dispatch(authenticateUser.loading());
-    return authClient
-      .login(loginRequest)
-      .then((response: TokenPayload) => {
-        AppAxiosInstance.defaults.headers['X-Access-Token'] =
-          response.accessToken;
+    return protectedApiClient
+      .createSchool(schoolRequest)
+      .then((response: SchoolResponse) => {
         dispatch(authenticateUser.loaded(response));
       })
       .catch((error) => {
