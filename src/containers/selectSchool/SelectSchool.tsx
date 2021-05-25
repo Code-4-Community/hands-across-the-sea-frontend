b7e3074118be0f99c8343913de9a2c6e7e248ee0
Ryan Jung <ryan.matthew.jung@gmail.com>
2021-05-25 16:31:33 -0400

    npm run check

--- src/containers/selectSchool/SelectSchool.tsx
+++ src/containers/selectSchool/SelectSchool.tsx
@@ -51,15 +51,21 @@ const SelectSchool: React.FC = () => {
                 <Col flex={24}>
                   <FormPiece note="Which school will you be monitoring today?">
                     <Form.Item name="schoolId" rules={[{ required: true }]}>
-                      <Select placeholder="Select a school"
-                      showSearch
-                      optionFilterProp="children"
-                      filterOption={(input, option: any) =>
-                        option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-                      }
-                      filterSort={(optionA, optionB) =>
-                        optionA.children.toLowerCase().localeCompare(optionB.children.toLowerCase())
-                      }>
+                      <Select
+                        placeholder="Select a school"
+                        showSearch
+                        optionFilterProp="children"
+                        filterOption={(input, option: any) =>
+                          option.children
+                            .toLowerCase()
+                            .indexOf(input.toLowerCase()) >= 0
+                        }
+                        filterSort={(optionA, optionB) =>
+                          optionA.children
+                            .toLowerCase()
+                            .localeCompare(optionB.children.toLowerCase())
+                        }
+                      >
                         {availableSchools.result.map(renderSchoolOption)}
                       </Select>
                     </Form.Item>
