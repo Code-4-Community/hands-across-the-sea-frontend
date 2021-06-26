3f17c0f266ff1b56a1ce00d3565c76cb8e2c3204
Ryan Jung <ryan.matthew.jung@gmail.com>
2021-06-26 16:00:45 -0400

    remove unused imports

--- src/containers/library-report/ducks/thunks.ts
+++ src/containers/library-report/ducks/thunks.ts
@@ -1,15 +1,13 @@
 import {
-  ReportWithLibraryRequest,
   LibraryReportThunkAction,
   LibraryReportResponse,
-  ReportWithoutLibraryRequest,
 } from './types';
 import { latestLibraryReport } from './actions';
 
 export const loadLatestLibraryReport = (
   schoolId: number,
 ): LibraryReportThunkAction<void> => {
-  return (dispatch, getState, { protectedApiClient }) => {
+  return (dispatch, _getState, { protectedApiClient }) => {
     dispatch(latestLibraryReport.loading());
     return protectedApiClient
       .getLatestReport(schoolId)
