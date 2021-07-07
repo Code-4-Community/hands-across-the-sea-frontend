40c6af83d5d88115d4a0dde54cf74f19d82d42a7
Max Sebso <maxsebso@gmail.com>
2021-07-07 13:35:43 -0700

    styling changes

--- src/components/report/PurposeOfVisit.tsx
+++ src/components/report/PurposeOfVisit.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Col, Form, Row, Select } from 'antd';
-import FormContainer from '../../components/form-style/FormContainer';
+import { Form, Select } from 'antd';
 import FormPiece from '../form-style/FormPiece';
 import { FormTextArea } from '../index';
 
@@ -27,34 +26,28 @@ const PurposeOfVisit: React.FC<PurposeOfVisitProps> = ({
   purposeOfVisit,
 }) => {
   return (
-    <FormContainer title="Reason for Visit">
-      <Row>
-        <Col flex={24}>
-          <FormPiece note="What is the purpose for today's visit?">
-            <Form.Item name="visitReason">
-              <Select
-                placeholder="Select a reason"
-                onChange={setPurposeOfVisitSelection}
-              >
-                {reasons.map((reason, i) => (
-                  <Select.Option value={reason} key={i}>
-                    {reason}
-                  </Select.Option>
-                ))}
-              </Select>
-            </Form.Item>
-            {purposeOfVisit === 'Other' && (
-              <Form.Item name={'otherVisitReason'}>
-                <FormTextArea
-                  placeholder="Please enter why here"
-                  autoSize={{ minRows: 4 }}
-                />
-              </Form.Item>
-            )}
-          </FormPiece>
-        </Col>
-      </Row>
-    </FormContainer>
+    <FormPiece note="What is the purpose for today's visit?" lastPiece>
+      <Form.Item name="visitReason">
+        <Select
+          placeholder="Select a reason"
+          onChange={setPurposeOfVisitSelection}
+        >
+          {reasons.map((reason, i) => (
+            <Select.Option value={reason} key={i}>
+              {reason}
+            </Select.Option>
+          ))}
+        </Select>
+      </Form.Item>
+      {purposeOfVisit === 'Other' && (
+        <Form.Item name={'otherVisitReason'}>
+          <FormTextArea
+            placeholder="Please enter why here"
+            autoSize={{ minRows: 4 }}
+          />
+        </Form.Item>
+      )}
+    </FormPiece>
   );
 };
 
