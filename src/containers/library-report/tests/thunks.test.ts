c415a6a614a1b40f0bba12614cf7e0c963f5300f
Derek <dleung2140@gmail.com>
2021-08-02 16:21:48 -0400

    fixed broken test

--- src/containers/library-report/tests/thunks.test.ts
+++ src/containers/library-report/tests/thunks.test.ts
@@ -43,6 +43,8 @@ describe('Report With Library Thunks', () => {
         actionPlans: null,
         successStories: null,
         timetable: null,
+        userName: '',
+        schoolName: '',
       };
 
       mockGetReportWithLibrary.mockResolvedValue(mockReportResponse);
