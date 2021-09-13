b163d1c57cf1f6b96f2253e4f7c28fda72565d99
Ryan Jung <ryan.matthew.jung@gmail.com>
2021-09-13 13:44:06 -0400

    replaced past submission schools

--- src/containers/pastSubmissionsSchools/ducks/reducers.ts
+++ src/containers/pastSubmissionsSchools/ducks/reducers.ts
@@ -1,45 +1,18 @@
-import {
-  ASYNC_REQUEST_FAILED_ACTION,
-  ASYNC_REQUEST_LOADED_ACTION,
-  ASYNC_REQUEST_LOADING_ACTION,
-  AsyncRequestNotStarted,
-  generateAsyncRequestReducer,
-} from '../../../utils/asyncRequest';
 import { C4CAction } from '../../../store';
+import { AsyncRequestNotStarted } from '../../../utils/asyncRequest';
+import { SET_PAST_SUBMISSIONS_SCHOOL_ID } from './actions';
 import {
   PastSubmissionsSchoolsReducerState,
   PastSubmissionsSchoolsResponse,
 } from './types';
-import {
-  pastSubmissionsSchools,
-  SET_PAST_SUBMISSIONS_SCHOOL_ID,
-} from './actions';
 
-export const initialPastSubmissionsSchools: PastSubmissionsSchoolsReducerState = {
-  pastSubmissionsSchools: AsyncRequestNotStarted<PastSubmissionsSchoolsResponse>(),
-};
-
-const pastSubmissionsSchoolsReducer = generateAsyncRequestReducer<
-  PastSubmissionsSchoolsReducerState,
-  PastSubmissionsSchoolsResponse,
-  void
->(pastSubmissionsSchools.key);
+export const initialPastSubmissionsSchools: PastSubmissionsSchoolsReducerState = {};
 
 const reducers = (
   state: PastSubmissionsSchoolsReducerState = initialPastSubmissionsSchools,
   action: C4CAction,
 ): PastSubmissionsSchoolsReducerState => {
   switch (action.type) {
-    case ASYNC_REQUEST_LOADING_ACTION:
-    case ASYNC_REQUEST_LOADED_ACTION:
-    case ASYNC_REQUEST_FAILED_ACTION:
-      return {
-        ...state,
-        pastSubmissionsSchools: pastSubmissionsSchoolsReducer(
-          state.pastSubmissionsSchools,
-          action,
-        ),
-      };
     case SET_PAST_SUBMISSIONS_SCHOOL_ID:
       return {
         ...state,
