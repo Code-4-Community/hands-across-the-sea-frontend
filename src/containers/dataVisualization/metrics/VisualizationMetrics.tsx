55ad4b397860387fb02aeaa93b5e40cbc91f078d
nate-kirschner <77256794+nate-kirschner@users.noreply.github.com>
2022-12-01 16:45:56 -0500

    Added selector to choose different graph types in Visualization tab

--- src/containers/dataVisualization/metrics/VisualizationMetrics.tsx
+++ src/containers/dataVisualization/metrics/VisualizationMetrics.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
-import { Row } from 'antd';
-import { Line } from '@ant-design/plots';
+import React, { useState } from 'react';
+import { Col, Row } from 'antd';
+import { Line, Column, Area, Pie } from '@ant-design/plots';
 import { useQuery } from 'react-query';
 import protectedApiClient from '../../../api/protectedApiClient';
 import moment from 'moment';
+import { StyledRow } from '../../../components/dataVisualization';
+import FormItemDropdown from '../../../components/form-style/FormItemDropdown';
+
+
+enum CHART_TYPES {
+  LINE = "Line",
+  COLUMN = "Column",
+  AREA = "Area",
+  PIE = "Pie"
+}
 
 const VisualizationMetrics: React.FC = () => {
+
+  const [selectedChartType, setSelectedChartType] = useState<string>("LINE");
+
   function formatDate(date: string) {
     return moment(date).format('MM/DD/YYYY');
   }
@@ -36,31 +49,85 @@ const VisualizationMetrics: React.FC = () => {
     getAggregatedData,
   );
 
-  return (
-    <Row justify="center">
-      {isLoading && <p>Loading visualization...</p>}
-      {error && <p>An error occurred loading visualization</p>}
-      {data && (
-        <Line
-          width={750}
-          data={data}
-          padding="auto"
-          xField="date"
-          yField="count"
-          xAxis={{
-            title: { text: 'Time' },
-          }}
-          yAxis={{
-            title: { text: 'Number of Books' },
-          }}
+  const displayChart = () => {
+    const config = {
+      width: 750,
+      data: data || [],
+      xAxis: {
+        title: { text: 'Time'}
+      },
+      yAxis: {
+        title: { text: 'Number of Books '}
+      },
+      xField: "date",
+      yField: "count"
+    }
+
+    if (selectedChartType === "LINE") {
+      return <Line 
+          {...config}
           tooltip={{
             formatter: (record) => {
               return { name: 'Number of Books', value: record.count };
             },
           }}
-        />
-      )}
-    </Row>
+      />
+    } else if (selectedChartType === "COLUMN") {
+      return <Column 
+          {...config}
+          tooltip={{
+            formatter: (record) => {
+              return { name: 'Number of Books', value: record.count };
+            },
+          }}
+      />
+    } else if (selectedChartType === "AREA") {
+      return <Area 
+          {...config}
+          tooltip={{
+            formatter: (record) => {
+              return { name: 'Number of Books', value: record.count };
+            },
+          }}
+      />
+    } else if (selectedChartType == "PIE") {
+      return <Pie 
+          {...config}
+          angleField='count'
+          colorField='date'
+          tooltip={{
+            formatter: (record) => {
+              return { name: 'Number of Books', value: record.count };
+            },
+          }}
+      />
+    }
+  }
+
+  return (
+    <>
+      <StyledRow justify="center">
+        <Col span={16}>
+          <FormItemDropdown
+            clarifyText={
+              'Select Graph Type'
+            }
+            optionsEnum={CHART_TYPES}
+            name={'chartType'}
+            text={'Line'}
+            disabled={false}
+            required={true}
+            onChange={(value) => setSelectedChartType(value)}
+          />
+        </Col>
+      </StyledRow>
+    
+      <Row justify="center">
+        {isLoading && <p>Loading visualization...</p>}
+        {error && <p>An error occurred loading visualization</p>}
+        {data && displayChart()}
+      </Row>
+    </>
   );
 };
 
